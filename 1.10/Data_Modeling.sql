484d36ab71d5c9ad599e4e974ffe12a4ed26fc37
Badmos Adesola <badmosayomide02@gmail.com>
2026-09-02 02:02:07 +0100

    Update SQL projects

--- 1.10/Data_Modeling.sql
+++ 1.10/Data_Modeling.sql
@@ -6,3 +6,16 @@ SELECT
 FROM
     job_postings_fact
 LIMIT 10;
+
+SELECT 
+    company_id,
+    name
+FROM
+    company_dim
+LIMIT 10;
+
+SELECT  *
+FROM
+    COMPANY_dim
+WHERE
+    name in ('Facebook', 'Metal', 'Google', 'Amazon', 'Apple');
