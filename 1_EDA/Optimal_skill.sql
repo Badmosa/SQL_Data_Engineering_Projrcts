1ce52f2037e9f51769fc28c2423c0af6b2703343
Badmos Adesola <badmosayomide02@gmail.com>
2026-09-03 02:28:00 +0100

    Update SQL projects

--- 1_EDA/Optimal_skill.sql
+++ 1_EDA/Optimal_skill.sql
@@ -6,26 +6,31 @@ Question: What are the most optimal skills for data engineers—balancing both d
     - This approach highlights skills that balance market demand and financial reward. It weights core skills appropriately instead of letting rare, outlier skills distort the results.
     - The natural log transformation ensures that both high-salary and widely in-demand skills surface as the most practical and valuable to learn for data engineering careers.
 */
+--ROUND function format counts to and decimal place--
+--MEDIAN function calculates the median value of a numeric column, which is less sensitive to outliers than the average. 
+--It provides a more accurate representation of the typical salary for each skill.--
 
 SELECT 
     sd.skills,
-    ROUND(MEDIAN(jpf.salary_year_avg), 0) AS median_salary,
-    COUNT(jpf.*) AS demand_count
-FROM
-    job_postings_fact AS jpf
-iNNER JOIN
-    skills_job_dim AS sjd
-    ON jpf.job_id = sjd.job_id
-INNER JOIN
-    skills_dim AS sd
-    ON sjd.skill_id = sd.skill_id
+    ROUND(MEDIAN(jpf.salary_year_avg), 1) AS median_salary,
+    COUNT(jpf.*) AS demand_count,
+    ROUND(LN(COUNT(jpf.*)), 1) AS ln_demand_count,
+    ROUND((LN(COUNT(jpf.*)) * MEDIAN(jpf.salary_year_avg))/1_000_000, 2) AS optimal_score
+FROM 
+    job_postings_fact jpf
+INNER JOIN 
+    skills_job_dim sjd ON jpf.job_id = sjd.job_id
+INNER JOIN 
+
+    skills_dim sd ON sjd.skill_id = sd.skill_id
 WHERE
     jpf.job_title_short = 'Data Engineer'
-    AND jpf.job_work_from_home = TRUE
+    AND jpf.salary_year_avg IS NOT NULL
+    AND jpf.job_work_from_home = True 
 GROUP BY
     sd.skills
-HAVING
-    COUNT(jpf.*) >= 100
+HAVING 
+    COUNT(sjd.job_id) >= 100
 ORDER BY
-    median_salary DESC        
+    optimal_score DESC
 LIMIT 25;  
