237c900901c52cc931ef77a66486e12468df61a3
Badmos Adesola <badmosayomide02@gmail.com>
2026-09-04 00:44:19 +0100

    Update SQL projects

--- 1.20/Data_Type.sql
+++ 1.20/Data_Type.sql
@@ -1,10 +1,12 @@
 SELECT CAST('123' AS INTEGER);
 
 SELECT
-    job_id,
+    CAST(job_id AS VARCHAR) || '-' || CAST(company_id AS VARCHAR),
     CAST(job_work_from_home AS INT) AS job_work_from_home,
     CAST(job_posted_date AS DATE) AS job_posted_date,
-    salary_year_avg
+    CAST(salary_year_avg AS DECIMAL(10, 0)) AS salary_year_avg
 FROM
-    job_postings_fact 
-LIMIT 10;       
+    job_postings_fact
+WHERE
+    salary_year_avg IS NOT NULL 
+LIMIT 10;
